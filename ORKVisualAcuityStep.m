7e723a906e3897fa2e9ff4a79049ba7831e4be69
flarmed <javierlara@me.com>
2015-05-07 17:25:30 +0200

    Initial Commit
    Initial commit

---
+++ ORKVisualAcuityStep.m
@@ -0,0 +1,13 @@
+//
+//  ORKVisualAcuityStep.m
+//  ResearchKit
+//
+//  Created by javier Lara Medina on 7/5/15.
+//  Copyright (c) 2015 researchkit.org. All rights reserved.
+//
+
+#import "ORKVisualAcuityStep.h"
+
+@implementation ORKVisualAcuityStep
+
+@end
